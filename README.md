6f39a8b1f14f05bf9f796fd81cf7323637c6918c
CosmodiumCS <73325248+CosmodiumCS@users.noreply.github.com>
2021-01-17 21:26:53 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Capulator
+
+> Chris Taylor [C0SM0] | 1/17/2021
+
+```
+![Capulator Logo](https://your-copied-image-address)
+```
+
+## Overview:
+```
+Introducing Capulator!
+A Full Functioning Calculator With A Hidden Vault [Built In]
+The Result of a 12-hour Programming Challenge
+```
+
+## Requirements:
+- python3 [pip]
+- Windows Operating System
+
+## Instructions:
+> Set-Up:
+- Use "installer.cmd" to Install All Dependencies
+    - If You Are Uncomfortable Using The Installer, Just:
+        - Move The "Capulator" File to your Program Data [C Drive]
+        ```bash
+        pip install Pillow
+        pip install cryptography
+        ```
+
+> Run:
+```bash
+python3 capulator.py
+```
+![Capulator Home](https://your-copied-image-address)
+
+> Access Vault:
+- To Access Vault, Enter "0000" Then Press "="
+    - Password Can Be Changed
---
+++ installer.cmd
@@ -0,0 +1,62 @@
+@REM get admin permissions for script
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> if error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+@REM start installation process
+
+@REM confirm installation
+echo Are you sure you want to install "Wized" (Y/N)
+set/p "cho=>"
+if %cho%==Y goto INSTALL
+if %cho%==y goto INSTALL
+if %cho%==n goto END
+if %cho%==N goto END
+echo Invalid choice.
+goto END
+
+@REM installs dependencies
+:INSTALL
+echo Installing Python Packages...
+pip3 install Pillow
+pip3 install cryptography
+echo Packages Installed!
+
+echo Moving Directories...
+move Capulator C:\ProgramData
+echo Direcrtories Moved!
+
+@REM echo Inastallation Complete, You May Start "capulator.py"
+@REM pause
+
+echo Starting "capulator.py"
+python3 capulator.py
+
+@REM exits installer
+:END
+exit
